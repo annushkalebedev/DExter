fixed data normalization for the new loading

task/diffusion.py
@@ -260,12 +260,15 @@ class CodecDiffusion(pl.LightningModule):
             total_loss += losses[k]
             self.log(f"Val/{k}", losses[k])           
 
-        sampled_loss, fig, tempo_vel_loss, tempo_vel_cor = self.predict(batch, batch_idx, save_animation=save_animation)
-        
-        self.logger.log_image(key=f"Val/tempo_curves", images=[fig])
-        self.log(f"Val/sampled_loss", sampled_loss)
-        self.log(f"Val/tempo_vel_loss", tempo_vel_loss)
-        self.log(f"Val/tempo_vel_cor", tempo_vel_cor)
+        # sample a fraction (1 percent) of the testing set
+        randgen = torch.rand(1)[0]
+        if randgen <= self.hparams.valid_fraction:
+            sampled_loss, fig, tempo_vel_loss, tempo_vel_cor = self.predict(batch, batch_idx, save_animation=save_animation)
+            
+            self.logger.log_image(key=f"Val/tempo_curves", images=[fig])
+            self.log(f"Val/sampled_loss", sampled_loss)
+            self.log(f"Val/tempo_vel_loss", tempo_vel_loss)
+            self.log(f"Val/tempo_vel_cor", tempo_vel_cor)
 
     def test_step(self, batch, batch_idx, save_animation=False):
 
@@ -299,6 +302,7 @@ class CodecDiffusion(pl.LightningModule):
                 # c_codec = 0.5 * batch['c_codec'][:8] + 0.5 * batch['c_codec'][8:] # average
             else:
                 start_noise = None
+
         else: # transfer in training
             start_noise = batch_source_codec
             c_codec = batch_label['c_codec'] - batch_source['c_codec']
@@ -364,7 +368,6 @@ class CodecDiffusion(pl.LightningModule):
     def step(self, batch, batch_idx):
         p_codec, s_codec, c_codec = batch['p_codec'], batch['s_codec'], batch['c_codec']
         batch_size = p_codec.shape[0]
-        assert(batch_size % 2 == 0)
         device = p_codec.device
 
         p_codec = p_codec.unsqueeze(1)  # (B, 1, T, F)

train.py
@@ -33,35 +33,27 @@ def main(cfg):
     cfg.data_root = to_absolute_path(cfg.data_root)
     
     # load our data
-    if cfg.dataloader.mixup:
-        if cfg.dataloader.smoothed:
-            codec_data = np.load(cfg.data_root + f"/codec_N={cfg.seg_len}_mixup_smoothed.npy", allow_pickle=True) # (N_data, 1000, 4)
-        else:
-            codec_data = np.load(cfg.data_root + f"/codec_N={cfg.seg_len}_mixup.npy", allow_pickle=True) # (N_data, 1000, 4)
-    else:
-        codec_data = np.load(cfg.data_root + f"/codec_N={cfg.seg_len}.npy", allow_pickle=True)
-    
-    # N = 1000: 23,190 - stats for ATEPP+ASAP+VIENNA
-    # N = 300: 68,981
-    # N = 200: 101,947
-    # N = 200_mixup: 1,213,088 (1.2M)
-    # N = 200_mixup filter (tempo and cep_features): 589,141 
-    # N = 100: 200,315 
-
-    # Normalize data
-    codec_data, means, stds = dataset_normalization(codec_data)
-    cfg.task.dataset_means = means
-    cfg.task.dataset_stds = stds
-
     if cfg.train_target == "transfer": # load only from paired set. 
         paired, _ = load_transfer_pair(K=2374872, N=cfg.seg_len) 
         train_set, valid_set = split_train_valid(paired, select=False)
-        train_loader = DataLoader(train_set, **cfg.dataloader.train)
-        val_loader = DataLoader(valid_set, **cfg.dataloader.val)            
-    else: # generation: keep 1000 pairs in validation but use unpaired for training.
-        paired, unpaired = load_transfer_pair(K=1000, N=cfg.seg_len) 
-        train_loader = DataLoader(unpaired, **cfg.dataloader.train)
-        val_loader = DataLoader(paired, **cfg.dataloader.val)    
+        assert(len(train_set) % 2 == 0)
+        assert(len(valid_set) % 2 == 0)   
+        cfg.dataloader.train.shuffle = False
+    else: # generation: keep 2000 pairs in validation but use unpaired for training.
+        paired, unpaired = load_transfer_pair(K=2000, N=cfg.seg_len) 
+        assert(len(paired) % 2 == 0)  # ~120 batch
+        train_set, valid_set = unpaired, paired
+
+    # Normalize data
+    train_set, valid_set, means, stds = dataset_normalization(train_set, valid_set)
+    cfg.task.dataset_means = means
+    cfg.task.dataset_stds = stds
+
+    train_loader = DataLoader(train_set, **cfg.dataloader.train)
+    val_loader = DataLoader(valid_set, **cfg.dataloader.val) 
+
+    # set the fraction so that around 20 batchs are output for listen.
+    cfg.task.valid_fraction = 20 / len(val_loader)      
 
     # Model
     if cfg.load_trained:

utils.py
@@ -19,7 +19,7 @@ import hook
 def tensor_pair_swap(x):
     if type(x) == list:
         x = np.array(x)
-    # given batched x, swap the pairs 
+    # given batched x, swap the pairs from the first dimension
     permute_index = torch.arange(x.shape[0]).view(-1, 2)[:, [1, 0]].contiguous().view(-1)
     return x[permute_index]
 
@@ -42,6 +42,7 @@ def render_sample(sampled_parameters,  batch_source, batch_label, save_path,
     # rescale the predictions and labels back to normal
     sampled_parameters = p_codec_scale(sampled_parameters, means, stds)
     batch_source['p_codec'] = p_codec_scale(batch_source['p_codec'], means, stds)
+    batch_label['p_codec'] = p_codec_scale(batch_label['p_codec'], means, stds)
 
     fig, ax = plt.subplots(int(B/2), 4, figsize=(24, 3*B))
     for idx in range(B): 
@@ -66,6 +67,8 @@ def render_sample(sampled_parameters,  batch_source, batch_label, save_path,
 
         pcodec_label = parameters_to_performance_array(batch_label['p_codec'][idx].cpu())
         pcodec_source = parameters_to_performance_array(batch_source['p_codec'][idx].cpu())
+
+        hook()
 
         if save_interpolation:
             pcodec_interpolate = torch.lerp(batch['p_codec'][idx].cpu(), batch['p_codec'][idx+B].cpu(), 0.5)
@@ -92,7 +95,6 @@ def render_sample(sampled_parameters,  batch_source, batch_label, save_path,
                                                     score, snote_ids, performance_array,
                                                     pcodec_label)
 
-
         tempo_vel_loss = F.l1_loss(torch.tensor(performed_tempo), torch.tensor(label_tempo)) + \
                                 F.l1_loss(torch.tensor(performed_vel), torch.tensor(label_vel)) 
 
@@ -100,7 +102,7 @@ def render_sample(sampled_parameters,  batch_source, batch_label, save_path,
 
         ax.flatten()[idx].plot(beats, performed_tempo, label="performed_tempo")
         ax.flatten()[idx].plot(beats, label_tempo, label="label_tempo")
-        ax.flatten()[idx].set_ylim(0, 500)
+        ax.flatten()[idx].set_ylim(0, 300)
 
         ax.flatten()[idx+B].plot(beats, performed_vel, label="performed_vel")
         ax.flatten()[idx+B].plot(beats, label_vel, label="label_vel")
@@ -281,10 +283,11 @@ def apply_normalization(cd, mean, std, i):
 
     return cd
 
-def dataset_normalization(codec_data):
+def dataset_normalization(train_set, valid_set):
     """ normalize the p_codec, across the dataset range. 
         return mean and std for each column. 
     """
+    codec_data = np.hstack([train_set, valid_set])
     dataset_pc = np.vstack([cd['p_codec'] for cd in codec_data])
 
     means, stds = [], []
@@ -298,7 +301,7 @@ def dataset_normalization(codec_data):
         means.append(float(mean))
         stds.append(float(std))  # conversion for save into OmegaConf
 
-    return codec_data, means, stds
+    return codec_data[:len(train_set)], codec_data[len(train_set):], means, stds
 
 def p_codec_scale(p_codec, means, stds):
     # inverse of normalization applied on p_codec 